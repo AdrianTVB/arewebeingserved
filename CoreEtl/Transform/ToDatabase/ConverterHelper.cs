30af413c260ea73c395eac6871d9aade80e9beb1
adrian.tedingvanberkhout <adrian.tedingvanberkhout@re-leased.com>
2019-09-07 14:58:15 +1200

    Fixed converterhelper

--- CoreEtl/Transform/ToDatabase/ConverterHelper.cs
+++ CoreEtl/Transform/ToDatabase/ConverterHelper.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.EntityFramework;
 using System;
 using System.Linq;
-using Domain.EntityFramework;
 
 namespace CoreEtl.Transform.ToDatabase
 {
@@ -13,22 +12,35 @@ namespace CoreEtl.Transform.ToDatabase
 			Organisation org = dbContext.Organisations.FirstOrDefault( o => o.Name.ToLower( ) == orgNameRefined );
 			if ( org == null )
 			{
-				dbContext.Organisations.Add( org = new Organisation { Name = orgName } );
+				dbContext.Organisations.Add( org = new Organisation( ) { Name = orgName } );
 				dbContext.SaveChanges( );
 			}
 			return org;
 		}
 
-		public Organisation GetOrCreateMeeting( creo_dbEntities dbContext, string meetingName, DateTime date )
+		public Meeting GetOrCreateMeeting( creo_dbEntities dbContext, string meetingName, DateTime date, Organisation organisation )
 		{
 			string meetingNameRefined = meetingName.ToLower( ).Trim( );
-			Organisation org = dbContext.Meetings.FirstOrDefault( o => o.Name.ToLower( ) == meetingNameRefined && o.);
+			Meeting org = dbContext.Meetings.FirstOrDefault( o => o.Name.ToLower( ) == meetingNameRefined && o.Date == date );
+			if ( org == null )
+			{
+				org = new Meeting( ) { Name = meetingName, Date = date, Organisation = organisation };
+				dbContext.Meetings.Add( org );
+				dbContext.SaveChanges( );
+			}
+			return org;
+		}
 
-			if (org != null)
-				return org;
-
-			dbContext.Organisations.Add( org = new Organisation( ) { Name = meetingName } );
-			dbContext.SaveChanges( );
+		public Official GetOrCreateOfficial( creo_dbEntities dbContext, string officialName, Organisation organisation )
+		{
+			string meetingNameRefined = officialName.ToLower( ).Trim( );
+			Official org = dbContext.Officials.FirstOrDefault( o => o.Name.ToLower( ) == meetingNameRefined );
+			if ( org == null )
+			{
+				org = new Official( ) { Name = officialName  };
+				dbContext.Officials.Add( org );
+				dbContext.SaveChanges( );
+			}
 			return org;
 		}
 	}
