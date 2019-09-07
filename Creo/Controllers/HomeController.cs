f56f3493780356ef3178b0909b747e0d08bdd374
gabriel <gabriel.ward@re-leased.com>
2019-09-08 10:18:22 +1200

    Update to modal

--- Creo/Controllers/HomeController.cs
+++ Creo/Controllers/HomeController.cs
@@ -26,24 +26,16 @@ namespace Creo.Controllers
 			{ "Wright", "https://napier.govt.nz/assets/Contacts/Councillor-Tania-Wright-Oct-2016-12.jpg" }
 
 		};
-		public ActionResult Index( )
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public PartialViewResult ListOfCouncillors( )
 		{
 			MeetingAttendanceList vm = new MeetingAttendanceList( );
 			vm.Attendees = new List<OfficialListItem>( );
-
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Boag", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Maxine-Boag-Oct-2016-5.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 2, Name = "Brosnan", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Annette-Brosnan-Oct-2016-7.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 2, Name = "Dallimore", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Larry-Dallimore-Oct-2016-8.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 2, Name = "Dalton", Attendances = 1, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Mayor-Bill-Dalton-Oct-2016-3.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Hague", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Claire-Hague-Oct-2016-6.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Jeffrey", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Tony-Jefferies-Mar-2017-2.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "McGrath", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Richard-McGrath-Oct-2016-2.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Price", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Keith-Price-Oct-2016-2.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Tapine", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/_resampled/FillWyI4MDAiLCI4MDAiXQ-Councillor-Apiata-Tapine-Oct-2016.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Taylor", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/_resampled/FillWyI4MDAiLCI4MDAiXQ-Councillor-Graeme-Taylor-Oct-2016-7.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "White", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Faye-White-Oct-2016.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Wise", Attendances = 4, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Kirsten-Wise-Mar-2017.jpg" } );
-			//vm.Attendees.Add( new OfficialListItem { Id = 1, Name = "Wright", Attendances = 1, ProfileImageUrl = "https://napier.govt.nz/assets/Contacts/Councillor-Tania-Wright-Oct-2016-12.jpg" } );
 
 			using ( creo_dbEntities dbContext = new creo_dbEntities( ) )
 			{
@@ -59,7 +51,7 @@ namespace Creo.Controllers
 
 			vm.MaxAttending = vm.Attendees.Select( a => a.Attendances ).Max();
 
-			return View( vm );
+			return PartialView( vm );
 		}
 
 		public ActionResult OverView( )
