dbb5c611687c792598f5987c832bfc3e353f72fe
kamal <kamal@redjungle.net>
2019-09-08 13:56:12 +1200

    added counsllers images for hastings

--- Creo/Controllers/HomeController.cs
+++ Creo/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Creo.Controllers
 {
 	public class HomeController : Controller
 	{
+
 		private static Dictionary<string, string> urlDict = new Dictionary<string, string>
 		{
 			{ "Boag", "https://napier.govt.nz/assets/Contacts/Councillor-Maxine-Boag-Oct-2016-5.jpg" },
@@ -23,13 +24,28 @@ namespace Creo.Controllers
 			{ "Taylor", "https://napier.govt.nz/assets/Contacts/_resampled/FillWyI4MDAiLCI4MDAiXQ-Councillor-Graeme-Taylor-Oct-2016-7.jpg" },
 			{ "White", "https://napier.govt.nz/assets/Contacts/Councillor-Faye-White-Oct-2016.jpg" },
 			{ "Wise", "https://napier.govt.nz/assets/Contacts/Councillor-Kirsten-Wise-Mar-2017.jpg" },
-			{ "Wright", "https://napier.govt.nz/assets/Contacts/Councillor-Tania-Wright-Oct-2016-12.jpg" }
+			{ "Wright", "https://napier.govt.nz/assets/Contacts/Councillor-Tania-Wright-Oct-2016-12.jpg" },
 
+			//Hastings
+			{ "Hazlehurst", "https://www.hastingsdc.govt.nz/assets/Contacts/SandraHazlehurst-HDCweb.jpg" },
+			{ "Kerr", "https://www.hastingsdc.govt.nz/assets/Contacts/TaniaKerr-HDCweb.jpg" },
+			{ "Barber", "https://www.hastingsdc.govt.nz/assets/Contacts/BaydenBarber-HDCweb.jpg" },
+			{ "Dixon", "https://www.hastingsdc.govt.nz/assets/Contacts/MalcolmDixon-HDCweb.jpg" },
+			{ "Harvey", "https://www.hastingsdc.govt.nz/assets/Contacts/DamonHarvey-HDCweb.jpg" },
+			{ "Heaps", "https://www.hastingsdc.govt.nz/assets/Contacts/RodHeaps-HDCweb.jpg" },
+			{ "Lawson", "https://www.hastingsdc.govt.nz/assets/Contacts/EileenLawson-HDCweb.jpg" },
+			{ "Lyons", "https://www.hastingsdc.govt.nz/assets/Contacts/GeorgeLyons-HDCweb.jpg" },
+			{ "Nixon", "https://www.hastingsdc.govt.nz/assets/Contacts/SimonNixon-HDCweb.jpg" },
+			{ "O’Keefe", "https://www.hastingsdc.govt.nz/assets/Contacts/HenareOkeefe-HDCweb.jpg" },
+			{ "Poulain", "https://www.hastingsdc.govt.nz/assets/Contacts/JacobyPoulain-HDCweb.jpg" },
+			{ "Redstone", "https://www.hastingsdc.govt.nz/assets/Contacts/AnnRedstone-HDCweb.jpg" },
+			{ "Travers", "https://www.hastingsdc.govt.nz/assets/Contacts/GeraldineTravers-HDCweb.jpg" },
+			{ "Watkins", "https://www.hastingsdc.govt.nz/assets/Contacts/KevinWatkins-HDCweb.jpg" },
 		};
 
-		public ActionResult Index()
+		public ActionResult Index( )
 		{
-			return View();
+			return View( );
 		}
 
 		public ActionResult ListOfCouncillors( int organisationid )
@@ -39,17 +55,21 @@ namespace Creo.Controllers
 
 			using ( creo_dbEntities dbContext = new creo_dbEntities( ) )
 			{
-				foreach ( Official official in dbContext.Officials )
+				foreach ( Official official in dbContext.Officials.Where( w => w.OrganisationId == organisationid ) )
 				{
 					string url = null;
 					urlDict.TryGetValue( official.Name, out url );
-					vm.Attendees.Add( new OfficialListItem { Id = official.Id, Attendances = official.Attendances.Count, Name = official.Name, ProfileImageUrl = url} );
+					if ( official.Attendances.Count > 2 )
+					{
+						vm.Attendees.Add( new OfficialListItem { Id = official.Id, Attendances = official.Attendances.Count, Name = official.Name, ProfileImageUrl = url } );
+
+					}
 				}
 			}
 
-			vm.Attendees = vm.Attendees.OrderByDescending( a => a.Attendances ).ToList(  );
+			vm.Attendees = vm.Attendees.OrderByDescending( a => a.Attendances ).ToList( );
 
-			vm.MaxAttending = vm.Attendees.Select( a => a.Attendances ).Max();
+			vm.MaxAttending = vm.Attendees.Select( a => a.Attendances ).Max( );
 
 			return View( vm );
 		}
