1ae0ae903278e91497247ed951e3a3f5ac44632a
kamal <kamal@redjungle.net>
2019-09-08 13:05:34 +1200

    Added wordcloud page

---
+++ Creo/Controllers/WordCloudController.cs
@@ -0,0 +1,36 @@
+﻿using Creo.ViewModels.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Creo.Controllers
+{
+	public class WordCloudController : Controller
+	{
+
+		private const int NapierCityCouncilId = 4;
+
+
+		// GET: WordCloud
+		public ActionResult Index( int organisationId )
+		{
+			List<WordCloudImage> images = new List<WordCloudImage>( );
+			string path = "~/Images";
+			path += "/Napier";
+			images.Add( new WordCloudImage( path + "/napier_" + 0 + ".png" ) );
+
+			if ( organisationId == NapierCityCouncilId )
+			{
+				ViewBag.Title = "Napier City Council";
+			}
+			else
+			{
+				ViewBag.Title = "Hastings District Council";
+			}
+
+			return View( images );
+		}
+	}
+}
